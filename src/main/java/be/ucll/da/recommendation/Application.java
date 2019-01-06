ac9016f8ce6bd0a5fcc06863c16bfc13b5c3ef1e
mario aquino <mario.aquino@student.ucll.be>
2019-01-06 14:39:47 +0100

    consul changes

--- src/main/java/be/ucll/da/recommendation/Application.java
+++ src/main/java/be/ucll/da/recommendation/Application.java
@@ -1,12 +1,13 @@
 package be.ucll.da.recommendation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
